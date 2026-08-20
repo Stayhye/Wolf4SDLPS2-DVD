a9ee74326e9915cd5ae1c1e5c7e29d1222a94b11
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-20 16:30:08 -0500

    Update id_sd.c

--- id_sd.c
+++ id_sd.c
@@ -377,8 +377,8 @@ void SD_SetPosition(int channel, int leftpos, int rightpos)
     {
         case sds_SoundBlaster:
 //            SDL_PositionSBP(leftpos,rightpos);
-            //Mix_SetPanning(channel, ((15 - leftpos) << 4) + 15,
-                ((15 - rightpos) << 4) + 15);
+//            Mix_SetPanning(channel, ((15 - leftpos) << 4) + 15,
+//                ((15 - rightpos) << 4) + 15);
             break;
     }
 }
